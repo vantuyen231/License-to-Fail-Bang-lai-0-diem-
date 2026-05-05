d32af0f6a5163a87c3b1042ab3696bca5196b8e9
vantuyen231 <dovantuyen21112002@gmail.com>
2026-05-05 15:26:41 +0700

    suspension settings

--- Assets/_Scripts/CarController.cs
+++ Assets/_Scripts/CarController.cs
@@ -6,8 +6,21 @@ using UnityEngine.InputSystem;
 
 public class CarController : TuyenMonoBehaviour
 {
-    private CarControls playerInputSystem;
+    [Header("References")]
     [SerializeField] protected Rigidbody rbCar;
+    [SerializeField] protected Transform[] rayPoints;
+    [SerializeField] protected LayerMask drivable;//Be mat xe co the chay
+
+    [Header("Suspention Settings")]
+    [SerializeField] protected float springStiffness;//Do cung lo xo
+    [SerializeField] protected float damperStiffness;
+    [SerializeField] protected float restLength;//Chieu dai nghi
+    [SerializeField] protected float springTravel;//Nen gian toi da
+    [SerializeField] protected float wheelRadius;//Ban kinh ban xe
+
+
+    [Header("Input")]
+    private CarControls playerInputSystem;
     [SerializeField] protected Vector2 move = Vector2.zero;
     [SerializeField] protected Vector2 carRotation = Vector2.zero;
     [SerializeField] protected Vector2 _straight = Vector2.zero;
@@ -15,7 +28,7 @@ public class CarController : TuyenMonoBehaviour
 
     protected override void Awake()
     {
-        
+
         playerInputSystem = new CarControls();
         rbCar = GetComponent<Rigidbody>();
     }
@@ -33,6 +46,10 @@ public class CarController : TuyenMonoBehaviour
         Drive(move.y);
         Turn(move.x);
     }
+    private void FixedUpdate()
+    {
+        Suspension();
+    }
 
     private void OnEnable()
     {
@@ -43,6 +60,41 @@ public class CarController : TuyenMonoBehaviour
     private void OnDisable()
     {
         playerInputSystem.Disable();
+    }
+
+    private void LoadRayPoint()
+    {
+
+    }
+    private void Suspension()
+    {
+        foreach (Transform rayPoint in rayPoints)
+        {
+            RaycastHit hit;
+            float maxLength = restLength + springTravel;
+
+            if (Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, drivable))
+            {
+                float currentSpringLenght = hit.distance - wheelRadius;
+                float springCompression = (restLength - currentSpringLenght) / springTravel;
+
+                float springVelocity = Vector3.Dot(rbCar.GetPointVelocity(rayPoint.position), rayPoint.up);
+                float damForce = damperStiffness * springVelocity;
+
+                float springForce = springStiffness * springCompression;
+
+                float netForce = springForce- damForce;
+
+                rbCar.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
+
+                Debug.DrawLine(rayPoint.position,hit.point, Color.red);
+            }
+            else
+            {
+                Debug.DrawLine(rayPoint.position,rayPoint.position+(wheelRadius + maxLength)* -rayPoint.up, Color.green);
+            }
+        }
+
     }
     private void Drive(float straightCar)
     {

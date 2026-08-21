87e5107fe0f8e4c1f7d7052a82980ff9db16be30
vantuyen231 <dovantuyen21112002@gmail.com>
2026-08-21 22:38:20 +0700

    feat: PoliceCar Fix wheel colider, add torque, steer and brakeTorque Wheel

--- Assets/_Scripts/PoliceCar/PoliceCarMoving.cs
+++ Assets/_Scripts/PoliceCar/PoliceCarMoving.cs
@@ -6,11 +6,20 @@ public class PoliceCarMoving : TuyenMonoBehaviour
 {
     [SerializeField] protected float moveSpeed = 25f;
     [SerializeField] protected float turnSpeed = 120f;
+
+    [SerializeField] protected float motorPower = 2000f;   
+    [SerializeField] protected float brakePower = 4000f;   
+    [SerializeField] protected float maxSteerAngle = 35f;
+
     [SerializeField] protected Transform centerOfMass;
     [SerializeField] protected CarManager carPlayer;
     [SerializeField] protected Rigidbody rb;
     [SerializeField] protected float currentForwardInput;
     [SerializeField] protected float currentTurnInput;
+
+    [Header("WheelCollider")]
+    [SerializeField] protected List<WheelCollider> wheelCollidersCtrl = new List<WheelCollider>();
+    [SerializeField] protected List<WheelTransformCtrl> wheelTransformsCtrl = new List<WheelTransformCtrl>();
 
     private void FixedUpdate()
     {
@@ -18,6 +27,41 @@ public class PoliceCarMoving : TuyenMonoBehaviour
         this.ApplySteering();
     }
 
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadWheelColliders();
+        this.LoadWheelTransform();
+        this.LoadRigidbody();
+        this.LoadTransform();
+    }
+
+    protected virtual void LoadWheelColliders()
+    {
+        if (this.wheelCollidersCtrl.Count > 0) return;
+        this.wheelCollidersCtrl.AddRange(GetComponentsInChildren<WheelCollider>());
+    }
+
+    protected virtual void LoadWheelTransform()
+    {
+        if (this.wheelTransformsCtrl.Count > 0) return;
+        this.wheelTransformsCtrl.AddRange(GetComponentsInChildren<WheelTransformCtrl>());
+
+    }
+
+    protected virtual void LoadRigidbody()
+    {
+        if(rb != null) return;
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(transform.name + ": LoadRigidbody", gameObject);
+    }
+
+    protected virtual void LoadTransform()
+    {
+        if (centerOfMass != null) return;
+        centerOfMass = GetComponent<Transform>();
+        Debug.Log(transform.name + ": LoadTransform", gameObject);
+    }
 
     public virtual void SetInputs(float forward, float turn)
     {
@@ -27,17 +71,38 @@ public class PoliceCarMoving : TuyenMonoBehaviour
 
     protected virtual void ApplyMotor()
     {
-        Vector3 forceDirection = transform.forward * (this.currentForwardInput * this.moveSpeed);
-        this.rb.AddForce(forceDirection, ForceMode.Acceleration);
+        //Vector3 forceDirection = transform.forward * (this.currentForwardInput * this.moveSpeed);
+        //this.rb.AddForce(forceDirection, ForceMode.Acceleration);
+        if (Mathf.Abs(this.currentForwardInput)>0.05f)
+        {
+            wheelCollidersCtrl[0].brakeTorque = 0;
+            wheelCollidersCtrl[1].brakeTorque = 0; 
+            wheelCollidersCtrl[2].brakeTorque = 0;
+            wheelCollidersCtrl[3].brakeTorque = 0;
+            float torque = this.currentForwardInput * this.motorPower;
+            wheelCollidersCtrl[0].motorTorque = torque;
+            wheelCollidersCtrl[1].motorTorque = torque;
+
+        }
+        else
+        {
+            wheelCollidersCtrl[0].motorTorque = 0;
+            wheelCollidersCtrl[1].motorTorque = 0;
+            wheelCollidersCtrl[0].brakeTorque = brakePower;
+            wheelCollidersCtrl[1].brakeTorque = brakePower;
+            wheelCollidersCtrl[2].brakeTorque = brakePower;
+            wheelCollidersCtrl[3].brakeTorque = brakePower;
+
+        }
+
     }
 
     protected virtual void ApplySteering()
     {
         if (Mathf.Abs(this.currentForwardInput) > 0.05f)
         {
-            float turn = this.currentTurnInput * this.turnSpeed * Time.fixedDeltaTime;
-            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-            this.rb.MoveRotation(this.rb.rotation * turnRotation);
+            wheelCollidersCtrl[2].steerAngle = this.currentTurnInput * this.turnSpeed;
+            wheelCollidersCtrl[3].steerAngle = this.currentTurnInput * this.turnSpeed;
         }
     }
     protected virtual void OnEnable()

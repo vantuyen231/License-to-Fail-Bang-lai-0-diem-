f4c98de7e9662a9352e49b8ea604f456a455571a
vantuyen231 <dovantuyen21112002@gmail.com>
2026-09-11 23:53:03 +0700

    feat: Mission conect logic mission to UI avoid obstacles Mission

--- Assets/_Scripts/GameMission/AvoidObstacles/MissionAvoidObstacles.cs
+++ Assets/_Scripts/GameMission/AvoidObstacles/MissionAvoidObstacles.cs
@@ -8,11 +8,15 @@ public class MissionAvoidObstacles : BaseMission
     [Header("Mission Configs")]
     [SerializeField] protected float timeMission = 30f;
     [SerializeField] protected int scoreMission = 100;
-    [SerializeField] protected float currentTimeTitle = 0f;
-    [SerializeField] protected float currentTimeReady = 0f;
     [SerializeField] protected float currentTimeMission = 0f;
     [SerializeField] protected float timeIntroTittle = 3f;
     [SerializeField] protected float timeReadyMission = 3f;
+
+    [SerializeField] protected int secondsLeft = 0;
+
+    [SerializeField] protected float currentReady = 0f;
+
+
 
     [Header("Mission States")]
     [SerializeField] protected bool isMissionActive = false;
@@ -21,7 +25,8 @@ public class MissionAvoidObstacles : BaseMission
     public static event Action OnStartMission;
     protected override void Start()
     {
-
+        base.Start();
+        this.StartMission();
     }
 
     protected virtual void OnEnable()
@@ -39,15 +44,29 @@ public class MissionAvoidObstacles : BaseMission
         Debug.Log("Hit, mission fail");
     }
 
-    protected virtual void SetReadyMission()
+    protected override void StartMission()
     {
-        Debug.Log("Mission Title");
-        currentTimeTitle += Time.deltaTime;
-        if(currentTimeTitle <= timeIntroTittle) return;
-        timeReadyMission -= Time.deltaTime;
-        if (timeReadyMission > 0f) return;
+        base.StartMission();
+        this.isMissionActive = false;
+        Debug.Log("StartMission");
+        StartCoroutine(this.SetReadyMission());
+    }
+    protected virtual IEnumerator SetReadyMission()
+    {
+        Debug.Log("Show Title");
+        yield return new WaitForSeconds(timeIntroTittle);
+        currentReady = this.timeReadyMission;
+        while (currentReady > 0)
+        {
+            secondsLeft = Mathf.CeilToInt(currentReady);
+
+            OnUpdateCountdown?.Invoke(secondsLeft);
+
+            yield return new WaitForSeconds(1f);
+            currentReady -= 1f;
+        }
         Debug.Log("Start Mision");
-
-
+        this.isMissionActive = true;
+        OnStartMission?.Invoke();
     }
 }
